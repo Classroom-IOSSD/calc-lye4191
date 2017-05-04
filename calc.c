57b064be5728c43eb8e54ed236e35eb06bd492d8
lye4191 <lye4191@naver.com>
2017-05-04 16:39:11 +0900

    Lab9

--- calc.c
+++ calc.c
@@ -8,25 +8,31 @@ int main(){
 	int line = 0;
 	double result = 0;
 	fp = fopen("read.txt","r");
+	double (*calc_function) (int,int);
+
 	if(fp!=NULL){
-		fscanf(fp, "%d", &line);
-	
+		fscanf(fp, "%d", &line);	
 		for(int i=0; i<line-1; i++) {
 			fscanf(fp, "%d %c %d",&operand1, &operator, &operand2);
 			switch(operator) {
 				case '+':
-				result = add(operand1, operand2);
+				//result = add(operand1, operand2);
+				calc_function = add;
 				break;
 				case '-':
-				result = minus(operand1, operand2);
+				//result = minus(operand1, operand2);
+				calc_function = minus;
 				break;
 				case '*':
-				result = mul(operand1, operand2);
+				//result = mul(operand1, operand2);
+				calc_function = mul;
 				break;	
 				case '/':
-				result = div(operand1, operand2);
+				calc_function = div;
+				//result = div(operand1, operand2);
 				break;
-			}		
+			}	
+			result = calc_function(operand1, operand2);	
 			printf("%d %c %d = %.6f\n",
 				 operand1, operator, operand2, result);
 
